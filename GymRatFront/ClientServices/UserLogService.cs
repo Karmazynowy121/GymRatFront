690e3804231f74306b9a9e7495a270faec37624b
Karmazynowy121 <atak121@wp.pl>
2023-01-26 15:48:09 +0100

    innit Register and Login created and Update changes plus Trainings page added

---
+++ GymRatFront/ClientServices/UserLogService.cs
@@ -0,0 +1,10 @@
+﻿using GymRatApi.Dto;
+
+namespace GymRatFront.ClientServices
+{
+    public class UserLogService
+    {
+        public bool IsLogIn { get; set; }
+        public UserDto User { get; set; }
+    }
+}
--- GymRatFront/Program.cs
+++ GymRatFront/Program.cs
@@ -1,3 +1,4 @@
+using GymRatFront.ClientServices;
 using GymRatFront.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -8,7 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<UserLogService>(o => 
+new UserLogService() { IsLogIn = true }); 
 
 
 var app = builder.Build();
